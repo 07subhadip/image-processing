a0c2d1259cba2e6dbce425f5f526ac0f5b676bbf
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-02 19:39:37 +0530

    changes done

---
+++ gaussian_img.py
@@ -0,0 +1,6 @@
+from skimage import io, filters
+from matplotlib import pyplot as plt
+img  = io.imread('E:\\Pictures\\Saved Pictures\\nature_edited.jpg')
+gaussian_img = filters.gaussian(img , sigma = 2)
+plt.imshow(gaussian_img)
+plt.show()
